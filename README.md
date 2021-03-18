58350d998eed832e60fcaa67f2967a2f1e820ce3
maxep <me@maxep.me>
2021-03-18 10:53:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Appetize Upload Action
+
+## Inputs
+
+|key|description|required|defaul|
+|---|---|---|---|
+|api-host|Appetize API host|[ ]|api.appetize.io|
+|api-token|Appetize.io API Token|[x]||
+|file-url|URL from which the app file can be fetched Alternative to `file-path`|[ ]||
+|platform|Either `ios` or `android`|[x]||
+|file-path|Path to app build on the local filesystem. Either this or `file-url` must be specified|[ ]||
+|public-key|If not provided, a new app will be created. If provided, the existing build will be overwritten|[ ]||
+|note|Notes you wish to add to the uploaded app|[ ]||
+|timeout|The number of seconds to wait until automatically ending the session due to user inactivity. Must be 30, 60, 90, 120, 180, 300, 600, 1800, 3600 or 7200. Default is 120|[ ]|120|
+
+```
+
+## Example usage
+```yml
+- uses: actions/checkout@v2
+
+- name: Build App
+  run: |
+    xcodebuild -project App.xcodeproj -scheme App -sdk iphonesimulator -derivedDataPath output
+    find output -name '*.app' -print | zip -r output/app.zip -@
+
+- name: Upload to Appetize.io
+  uses: maxep/github-action-appetize@0.1.0
+  with:
+      api-token: ${{ secrets.APPETIZE_TOKEN }} 
+      file-path: output/app.zip
+      platform: 'ios'
+```
---
+++ index.js
@@ -0,0 +1,92 @@
+const core = require('@actions/core')
+const fs = require('fs')
+const axios = require("axios")
+const FormData = require("form-data")
+
+const main = async () => {
+
+  const args = {
+    host: core.getInput('api-host'),
+    token: core.getInput('api-token', { required: true }),
+    url: core.getInput('file-url'),
+    platform: core.getInput('platform', { required: true }),
+    file: core.getInput('file-path'),
+    publicKey: core.getInput('public-key'),
+    note: core.getInput('note'),
+    timeout: core.getInput('timeout'),
+  }
+
+  if (!args.url && !args.path) 
+    throw 'Either `file-path` or `file-url` must be specified'
+
+  if (args.platform !== 'ios' && args.platform !== 'android') 
+    throw '`platform` is either `ios` or `android`'
+
+  /**
+   * Build the The Appetize API URL.
+   * @returns The URL.
+   */
+  const appetize = () => {
+    const url = `https://${args.host}/v1/apps/`
+    if (args.publicKey) return url + args.publicKey
+    return url
+  }
+
+  /**
+   * Create or Update using a publicly accessible link.
+   * @returns The Appetize API response.
+   */
+  const post = async () => {
+    const params = {
+      url: args.url,
+      platform: args.platform,
+      note: args.note,
+      timeout: args.timeout
+    }
+  
+    return await axios.post(appetize(), params, {
+        auth: { username: args.token, password: '' }
+      }
+    )
+  }
+
+  /**
+   * Direct file uploads
+   * @returns The Appetize API response.
+   */
+  const upload = async () => {
+    const form = new FormData()
+    form.append('file', fs.createReadStream(args.path))
+
+    if (args.platform)  form.append('platform', args.platform)
+    if (args.note)      form.append('note', args.note)
+    if (args.timeout)   form.append('timeout', args.timeout)
+  
+    const result = await axios.post(appetize(), form, {
+        auth: { username: args.token, password: '' },
+        headers: form.getHeaders(),
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity
+      }
+    )
+
+    fs.unlinkSync(args.path)
+    return result
+  }
+
+  /**
+   * Upload app to Appetize.
+   * @returns The upload result.
+   */
+  const run = async () => {
+    if (args.url) return await post()
+    if (args.path) return await upload()
+  }
+
+  const { data } = await run()
+  core.setOutput('APPETIZE_APP_URL', data.appURL)
+  core.setOutput('APPETIZE_MANAGE_URL', data.manageURL)
+  core.setOutput('APPETIZE_PUBLIC_KEY', data.publicKey)
+}
+
+main().catch(err => core.setFailed(err.message))
